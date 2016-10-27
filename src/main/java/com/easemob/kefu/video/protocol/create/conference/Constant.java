61d6beea72a69082fddf5b8c1aefd4098104c513
Wang Chunye <wcy123@gmail.com>
2016-10-27 11:49:18 +0800

    修改 IDEA产生的 warning

--- src/main/java/com/easemob/kefu/video/protocol/create/conference/Constant.java
+++ src/main/java/com/easemob/kefu/video/protocol/create/conference/Constant.java
@@ -2,4 +2,6 @@ package com.easemob.kefu.video.protocol.create.conference;
 
 public class Constant {
     public static final String KEY = "DDAY_101_airborne_division_e_company:We_stand_alone_together";
+
+    private Constant() {}
 }
